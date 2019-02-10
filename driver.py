8f9272426d6321a272ab4f028af9ba99192601b2
Gabe <gabe@databaseguy.com>
2019-02-09 16:32:58 -0800

    Added Messaging
    Drones can now message each other to combine their maps, based on whoever has the newest information.

--- driver.py
+++ driver.py
@@ -6,17 +6,18 @@ from drone import Drone
 
 class Driver(tk.Canvas):
 
-    NUM_DRONES = 5
-    TURN_TIME = 1000
-    X_DIM = 10
-    Y_DIM = 10
+    NUM_DRONES = 2
+    TURN_TIME = 2000
+    X_DIM = 2
+    Y_DIM = 2
 
     HEIGHT = 600
     WIDTH = 600
 
     graphics = {}
+    obstacle_graphics = {}
     drones = []
-    board = {(1, 3): "X", (4, 2): "X", (2, 4): "X"}
+    board = {(1, 3): "X", (4, 2): "X", (2, 4): "X", (9, 4): "X", (9, 8): "X"}
 
     drones_made = 0
 
@@ -26,11 +27,11 @@ class Driver(tk.Canvas):
         self.start()
 
     def start(self):
-        draw_lines()
+        self.draw_lines()
 
-        build_drones()
+        self.build_drones()
 
-        create_graphics()
+        self.create_graphics()
 
         print("Visualizations created. Starting simulation.")
         self.after(self.TURN_TIME, self.update)
@@ -56,7 +57,8 @@ class Driver(tk.Canvas):
         print("Building drones.")
         for i in range(self.NUM_DRONES):
             tries = 0
-            while tries == 0 or (tries < 10 and self.crashed(drone)):
+            while (tries == 0 or (tries < 10 and
+                  (self.drone_collision(drone) or self.obstacle_crash(drone)))):
                 tries += 1
                 drone = AbsDrone(
                     Drone(self.drones_made),
@@ -70,13 +72,12 @@ class Driver(tk.Canvas):
     def create_graphics(self):
         print("Drones built. Creating visualizations.")
 
-        for obstacle in board:
-            if board[obstable] == "X":
-                self.graphics[obstacle] =
-                    self.create_oval(
-                        *self.get_drone_draw_pos(*obstacle)[0], fill="#4444ff")
+        for obstacle in self.board:
+            if self.board[obstacle] == "X":
+                self.obstacle_graphics[obstacle] = self.create_oval(
+                    *self.get_rect_and_mid(*obstacle)[0], fill="#4444ff")
 
-        for drone in drones:
+        for drone in self.drones:
             drone_draw_pos = self.get_drone_draw_pos(drone)
             d_rect = self.create_rectangle(
                 *drone_draw_pos[0],
@@ -88,51 +89,78 @@ class Driver(tk.Canvas):
             self.graphics[drone] = (d_rect, d_text)
 
     def update(self):
-        new_board = copy(self.board)
-        self.move_drones(new_board)
+        new_board = self.board.copy()
+        # Clear all drones from board, to avoid
+        for k in list(new_board):
+            if new_board[k] != "X":
+                del new_board[k]
 
-        self.commit_drones(new_board)
+        # Use list() to allow modification during iteration, in case of crash.
+        for drone in list(self.drones):
+            map = self.make_map(drone)
 
-        self.board = new_board
-        self.draw()
+            move = drone.drone.update(map, self.send_message)
 
-    def move_drones(self, new_board):
-        for drone in self.drones:
-            map = {}
-            directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-            for d in directions:
-                char = 'O'
-                if (drone.x + d[0], drone.y + d[1]) in self.board:
-                    char = 'X'
-                map[d] = char
-            move = drone.move(map)
-            del new_board[(drone.x, drone.y)]
             drone.x += move[0]
             drone.y += move[1]
+            if self.obstacle_crash(drone):
+                self.destroy_drone(drone)
+            else:
+                # Note later drones can overwrite earlier ones here. This is
+                # semi-intentional, and the collision method ensures that all
+                # drones except the one with current claim to the tile will
+                # crash, which seems reasonable.
+                new_board[(drone.x, drone.y)] = str(drone.drone.num)
+        self.board = new_board
+        for drone in list(self.drones):
+            if self.drone_collision(drone):
+                self.destroy_drone(drone)
 
-    def commit_drones(self, new_board):
-        for drone in list(self.drones.keys()):
-            if self.crashed(drone):
-                for i in self.graphics[drone]:
-                    self.delete(i)
-                del self.board[(drone.x, drone.y)]
-                del self.graphics[drone]
-                del self.drones[drone].drone
-                del self.drones[drone]
-            new_board[(drone.x, drone.y)] = "D"
+        self.draw()
+
+    def send_message(self, to):
+        return self.get_drone(to).msg
+
+    def make_map(self, drone):
+        map = {}
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0), (1, 1), (-1, 1),
+            (1, -1), (-1, -1), (0, 2), (0, -2), (2, 0), (-2, 0)]
+        for d in directions:
+            char = 'O'
+            new_x = drone.x + d[0]
+            new_y = drone.y + d[1]
+            if (new_x >= self.X_DIM or new_y >= self.Y_DIM
+            or new_x < 0 or new_y < 0):
+                char = 'X'
+            if (new_x, new_y) in self.board:
+                char = self.board[(new_x, new_y)]
+            map[d] = char
+        return map
+
+    def get_drone(self, n):
+        drone = list(filter(lambda d: d.num == int(n), [d.drone for d in self.drones]))
+        if not drone:
+            self.debug_dump(drone = drone)
+            raise RuntimeError("Could not find drone #{}".format(n))
+        else:
+            return drone[0]
 
     def draw(self):
         for drone in self.graphics:
+
             drone_draw_pos = self.get_drone_draw_pos(drone)
             self.coords(self.graphics[drone][0], *drone_draw_pos[0])
             self.coords(self.graphics[drone][1], *drone_draw_pos[1])
         self.after(self.TURN_TIME, self.update)
 
     def get_drone_draw_pos(self, drone):
+        return self.get_rect_and_mid(drone.x, drone.y)
+
+    def get_rect_and_mid(self, x, y):
         rect_width = self.WIDTH / self.X_DIM
         rect_height = self.HEIGHT / self.Y_DIM
-        drone_x_mid = (drone.x + 0.5) * rect_width
-        drone_y_mid = (drone.y + 0.5) * rect_height
+        drone_x_mid = (x + 0.5) * rect_width
+        drone_y_mid = (y + 0.5) * rect_height
 
         d_rect = (
             drone_x_mid - 0.4 * rect_width,
@@ -146,29 +174,63 @@ class Driver(tk.Canvas):
             )
         return (d_rect, d_text)
 
-    def crashed(self, drone):
+    def obstacle_crash(self, drone):
         # Check for collisions
-        if (drone.x, drone.y) in self.board:
-            object_present = self.board[drone.x, drone.y]
+        drone_loc = (drone.x, drone.y)
+        if drone_loc in self.board:
+            object_present = self.board[drone_loc]
             if object_present == "X":
-                print("Drone {} crashed into an obstacle!".format(drone.num))
-                return True
-            elif object_present != str(drone.drone.num):
-                print("Drone {} crashed into drone {}!".format(drone.drone.num, object_present))
+                print("Drone {} crashed into an obstacle at {}!"
+                    .format(drone.drone.num, drone_loc))
                 return True
 
         # Check for out-of-bounds
-        if (self.drones[drone].x < 0
-          or self.drones[drone].y < 0
-          or self.drones[drone].x >= self.X_DIM
-          or self.drones[drone].y >= self.Y_DIM):
-            print("Drone {} went out of bounds and crashed!".format(drone.num))
+        if (drone.x < 0 or drone.y < 0
+            or drone.x >= self.X_DIM or drone.y >= self.Y_DIM):
+            print("Drone {} went out of bounds at {} and crashed!"
+                .format(drone.drone.num, drone_loc))
             return True
 
         return False
+
+    def drone_collision(self, drone):
+        # Check for collisions
+        for d in self.drones:
+            if ((drone.x, drone.y) == (d.x, d.y)
+                and drone.drone.num != d.drone.num):
+                # All drones except the last one to move into a tile crash.
+                if self.board[(drone.x, drone.y)] != str(drone.drone.num):
+                    print("Drone {} crashed into drone {}!"
+                        .format(drone.drone.num, d.drone.num))
+                    return True
+
+        return False
+
+    def destroy_drone(self, drone):
+        print("Destroying drone {}!".format(drone.drone.num))
+        for i in self.graphics[drone]:
+            self.delete(i)
+        del self.graphics[drone]
+        self.drones.remove(drone)
+        drone_loc = (drone.x, drone.y)
+        if (drone_loc in self.board
+          and self.board[drone_loc] == str(drone.drone.num)):
+            del self.board[drone_loc]
+
+    def debug_dump(self, **kwargs):
+        print("Board: {}\n".format(self.board))
+        print("Drones: {}\n".format([str(d) for d in self.drones]))
+        print("Drone Graphics: {}\n".format(
+            ["{}: {}".format(str(d), v) for d, v in self.graphics.items()]))
+        print("Obstacle Graphics: {}\n".format(self.obstacle_graphics))
+        for k,v in kwargs.items():
+            print("{}: {}\n".format(k, v))
 
 class AbsDrone():
     def __init__(self, drone, x, y):
         self.drone = drone
         self.x = x
         self.y = y
+
+    def __str__(self):
+        return "#{}: ({}, {})".format(self.drone.num, self.x, self.y)
--- drone.py
+++ drone.py
@@ -1,3 +1,5 @@
+import random
+import ast
 
 class Drone():
 
@@ -5,10 +7,138 @@ class Drone():
         self.num = num
         self.x = 0
         self.y = 0
-        self.map = {(0, 0): "M"}
+        self.t = 0
+        self.last_move = (0, 0)
+        self.map = {(0, 0): ['M', 0]}
+        self.syncs = {}
+        self.choreographed_moves = {}
+
+    def update(self, unprocessed_map, msg_callback):
+        map = self.process_map(unprocessed_map, (self.x, self.y))
+        # We have to move first, so we know which way to choreograph.
+        move = self.move(map)
+        self.message(map, msg_callback)
+        return move
+
+    def message(self, map, msg_callback):
+        nearby = self.get_nearby(map)
+        messages = []
+        for n in nearby:
+            print("Drone #{} messaging drone #{}.".format(self.num, n))
+            messager = msg_callback(n)
+            messager("MOVE" + str(self.num) + str(self.last_move))
+            messager("MAP" + str(self.num) + str(self.map))
 
     def move(self, map):
-        return (1, 0)
+        self.t += 1
+        self.update_map(map)
+        options = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+        random.shuffle(options)
+        while options:
+            dir = options.pop()
+            if self.map[(self.x + dir[0], self.y + dir[1])][0] == 'O': #self.move_is_safe(dir, map):
+                self.map[(self.x, self.y)] = ['O', self.t]
+                self.x += dir[0]
+                self.y += dir[1]
+                self.map[(self.x, self.y)] = ['M', self.t]
+                self.last_move = dir
+                break
+        # If we didn't break out of the loop, we can't move.
+        else:
+            self.last_move = (0, 0)
+        return self.last_move
+
+    # TODO: Handle choreographs.
+    def move_is_safe(self, dir, map):
+        pass
+
+
+    def msg(self, msg):
+        print("Drone #{} received message: {}".format(self.num, msg))
+        if msg[:4] == "MOVE":
+            msg = msg[4:]
+            dir_start = msg.find('(')
+            num = msg[:dir_start]
+            dir = msg[dir_start:]
+            self.choreographed_moves[num] = ast.literal_eval(dir)
+        elif msg[:3] == "MAP":
+            msg = msg[3:]
+            map_start = msg.find('{')
+            num = msg[:map_start]
+            unprocessed_map = msg[map_start:]
+            unprocessed_map = ast.literal_eval(unprocessed_map)
+            self.combine_maps(unprocessed_map, num)
+
+    # Requires a sensor map, not the memory map.
+    # TODO: Fix this so that it works with memory map instead.
+    def get_nearby(self, map):
+        print("#{}: {}\n".format(self.num, map))
+        near = []
+        for k, v in map.items():
+            if v[0] != 'X' and v[0] != 'M' and v[0] != 'O':
+                near.append(v)
+                print("Drone {} saw drone {}!".format(self.num, v))
+        return near
+
+    def process_map(self, unprocessed_map, offset=(0, 0)):
+        offset_x = offset[0]
+        offset_y = offset[1]
+        new_map = {}
+        for dir in unprocessed_map:
+            adjusted_x  = dir[0] + offset_x
+            adjusted_y = dir[1] + offset_y
+            adjusted_loc = (adjusted_x, adjusted_y)
+            if adjusted_loc != (0, 0):
+                new_map[adjusted_loc] = unprocessed_map[dir]
+        return new_map
+
+    def find_object(self, map, id):
+        for k, v in map.items():
+            if v[0] == id:
+                return k
+        raise RuntimeError("Could not find key {} in map {}".format(id, map))
+
+    def update_map(self, map):
+        for dir in map:
+            self.map[(dir[0], dir[1])] = (map[dir], self.t)
+
+    def combine_maps(self, unprocessed_map, num):
+        my_pos_to_them = self.find_object(unprocessed_map, str(self.num))
+        their_pos = self.find_object(unprocessed_map, 'M')
+        # Update their location to have their ID, instead of 'M'.
+        unprocessed_map[their_pos] = [str(num), unprocessed_map[their_pos][1]]
+
+        offset_x = self.x - my_pos_to_them[0]
+        offset_y = self.y - my_pos_to_them[1]
+
+        map = self.process_map(unprocessed_map, (offset_x, offset_y))
+        for dir in map:
+            if not dir in self.map or map[dir][1] > self.map[dir][1]:
+                self.map[dir] = map[dir]
+
+    def make_abs_map(self):
+        max_x = max([m[0] for m in self.map])
+        min_x = min([m[0] for m in self.map])
+        max_y = max([m[1] for m in self.map])
+        min_y = min([m[1] for m in self.map])
+        empty_map = [['?' for _ in range(min_x, max_x+1)] for _ in range(min_y, max_y + 1)]
+        for k, v in self.map.items():
+            x_rel = k[0]
+            y_rel = k[1]
+            x_abs = x_rel - min_x
+            y_abs = y_rel - min_y
+            y_tot = max_y - min_y
+            x_tot = max_x - min_x
+            empty_map[y_tot - y_abs][x_abs] = v[0]
+
+        return empty_map
+
+    def print_map(self):
+        abs_map = self.make_abs_map()
+        s = ""
+        for a in abs_map:
+            s += ''.join(a) + "\n"
+        print(s)
 
     def __hash__(self):
         return self.num
